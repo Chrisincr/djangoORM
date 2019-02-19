cb4b3a5da8a31cec9a9cb1ba5a2d921252572211
Your Name <you@example.com>
2019-02-19 16:20:16 -0600

    finished converted books and authors to full stack

--- book_authors_proj/apps/books_authors_app/urls.py
+++ book_authors_proj/apps/books_authors_app/urls.py
@@ -3,5 +3,12 @@ from django.urls import path
 from . import views
                     
 urlpatterns = [
-    url(r'^$', views.index),
+    url(r'^$', views.books),
+    url(r'^addbook$', views.newbook),
+    path('book/<int:id>', views.book),
+    path('book/addauthor', views.addauthor),
+    path('authors',views.authors),
+    path('addauthor', views.newauthor),
+    path('author/<int:id>', views.author),
+    path('author/addbook', views.addbook)
 ]
